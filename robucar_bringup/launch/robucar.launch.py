8833f05ec30544d883d8e0f6a625a000b5a1bf5e
jean.laneurit <jean.laneurit@inrae.fr>
2022-10-13 14:58:36 +0200

    robucar_brinup : fix cmd_mux out topic remapping

--- robucar_bringup/launch/robucar.launch.py
+++ robucar_bringup/launch/robucar.launch.py
@@ -196,7 +196,7 @@ def launch_setup(context, *args, **kwargs):
         executable="cmd_mux_node",
         name="cmd_mux",
         parameters=[{"topics_type": command_message_type}],
-        remappings=[("~/out", "cmd_two_axle_steering")],
+        remappings=[("~/out", "controller/cmd_two_axle_steering")],
         output="screen",
     )
 
@@ -238,90 +238,3 @@ def generate_launch_description():
     return LaunchDescription(
         declared_arguments + [OpaqueFunction(function=launch_setup)]
     )
-
-
-#+from launch import LaunchDescription
-#from launch.actions import IncludeLaunchDescription
-#from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-#from launch.launch_description_sources import PythonLaunchDescriptionSource
-#from ament_index_python.packages import get_package_share_directory
-
-#from launch_ros.actions import Node
-#from launch_ros.substitutions import FindPackageShare
-
-#def generate_launch_description():
-
-#    gazebo = IncludeLaunchDescription(
-#        PythonLaunchDescriptionSource(
-#            [PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]
-#        ),
-#        launch_arguments={"verbose": "false"}.items(),
-#    )
-
-#    info_file = get_package_share_directory("robucar_description") + "/config/robucar.yaml"
-#    urdf_file = get_package_share_directory("robucar_description") + "/urdf/robucar.urdf.xacro"
-#    controller_manager_yaml_file = get_package_share_directory("robucar_bringup") + "/config/controller_manager.yaml"
-
-#    print(controller_manager_yaml_file)
-#    mobile_base_controller_yaml_file = get_package_share_directory("aroco_bringup") + "/config/mobile_base_controller.yaml"
-
-#    # Get URDF via xacro
-#    robot_description_content = Command(
-#        [
-#            PathJoinSubstitution([FindExecutable(name="xacro")]),
-#            " ",
-#            PathJoinSubstitution(
-#                [
-#                    FindPackageShare("robucar_description"),
-#                    "urdf",
-#                    "robucar.urdf.xacro",
-#                ]
-#            ),
-#            " use_sim:=true ",
-#            "controller_conf_yaml_file:=",
-#            controller_manager_yaml_file,
-#        ]
-#    )
-#    robot_description = {"robot_description": robot_description_content}
-
-
-#    robot_state_publisher = Node(
-#        package="robot_state_publisher",
-#        executable="robot_state_publisher",
-#        output="screen",
-#        parameters=[robot_description],
-#    )
-
-#    spawn_entity = Node(
-#        package="gazebo_ros",
-#        executable="spawn_entity.py",
-#        arguments=["-topic", "robot_description", "-entity", "robucar"],
-#        output="screen",
-#    )
-
-#    joint_state_broadcaster = Node(
-#        package="controller_manager",
-#        executable="spawner.py",
-#        arguments=["joint_state_broadcaster"],
-#        output="screen",
-#    )
-
-#    mobile_base_controller = Node(
-#        package="controller_manager",
-#        executable="spawner",
-#        name="mobile_base_controller_spawner",
-#        arguments=["mobile_base_controller","--param-file",mobile_base_controller_yaml_file],
-#        output="screen",
-#    )
-
-
-#    return LaunchDescription(
-#        [
-#            gazebo,
-#            robot_state_publisher,
-#            spawn_entity,
-#            joint_state_broadcaster,
-#            mobile_base_controller,
-##            spawn_controller,
-#        ]
-#    )
