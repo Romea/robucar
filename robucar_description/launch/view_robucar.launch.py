969edbf94af58f12e44f803ccec82d46e9fbd360
jean.laneurit <jean.laneurit@inrae.fr>
2022-11-15 11:32:46 +0100

    robucar_description :  add python script able to create urdf description

--- robucar_description/launch/view_robucar.launch.py
+++ robucar_description/launch/view_robucar.launch.py
@@ -1,16 +1,29 @@
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from ament_index_python.packages import get_package_share_directory
+
+
 def generate_launch_description():
 
-    romea_mobile_base_description_package_prefix = get_package_share_directory('romea_mobile_base_description')
-    urdf_file = get_package_share_directory("robucar_description") + "/urdf/robucar.urdf.xacro"
+    romea_mobile_base_description_package_prefix = get_package_share_directory(
+        "romea_mobile_base_description"
+    )
+    urdf_file = (
+        get_package_share_directory("robucar_description") + "/urdf/robucar.urdf.xacro"
+    )
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([romea_mobile_base_description_package_prefix,'/launch/view_robot.launch.py']),
-            launch_arguments = {'urdf_file': urdf_file}.items(),
-        ),
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        romea_mobile_base_description_package_prefix,
+                        "/launch/view_robot.launch.py",
+                    ]
+                ),
+                launch_arguments={"urdf_file": urdf_file}.items(),
+            )
+        ]
+    )
---
+++ robucar_description/python/robucar_description.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def urdf(prefix, mode, controller_conf_yaml_file):
+
+    xacro_file = (
+        get_package_share_directory("robucar_description")
+        + "/urdf/robucar.urdf.xacro"
+    )
+
+    urdf_xml = xacro.process_file(
+        xacro_file,
+        mappings={
+            "prefix": prefix,
+            "mode": mode,
+            "controller_conf_yaml_file": controller_conf_yaml_file,
+        },
+    )
+
+    return urdf_xml.toprettyxml()
